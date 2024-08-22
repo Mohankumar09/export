8a4e07cbad085a8ea81cffd9349b783b1901ee3a
Your Name <you@example.com>
2024-08-22 14:37:28 +0530

    components created

--- export/src/app/app.module.ts
+++ export/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OurProductsComponent } from './our-products/our-products.component';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactUsComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarComponent,
+    OurProductsComponent,
+    ScrollToTopComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
